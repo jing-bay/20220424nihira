867bfe2ee02cf0cd4705ea253fe0f0cb1befc43e
jing-bay <figue@hotmail.co.jp>
2022-05-01 15:45:28 +0900

    update5

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -5,21 +5,23 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Models\User;
+use App\Models\Todo;
 
 class HelloTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testHello()
     {
-        User::factory()->create([
+        Todo::factory()->create([
             'content'=>'aaa',
-            'created_at'=>'2121-22-22',
-            'updated_at'=>'2121-22-22'
+            'created_at'=>'2222-22-22 00:00:00',
+            'updated_at'=>'2222-22-22 00:00:00'
         ]);
-        $this->assertDatabaseHas('users',[
+        $this->assertDatabaseHas('todos',[
             'content'=>'aaa',
-            'created_at'=>'2121-22-22',
-            'updated_at'=>'2121-22-22'
+            'created_at'=>'2222-22-22 00:00:00',
+            'updated_at'=>'2222-22-22 00:00:00'
         ]);
-    } 
+    }
 }
